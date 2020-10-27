ae2102a5ec37d1a0a60f64060c78e461100818da
Ha-sake <57680069+Ha-sake@users.noreply.github.com>
2020-10-27 19:15:16 +0000

    Pre-load balancing

--- pgrepwc.py
+++ pgrepwc.py
@@ -20,34 +20,47 @@ def main(argv):
         print("Utilização: pgrepwc [-c|-l] [-p n] palavra {ficheiros}")
         sys.exit(2)
 
-    wc, lc = matchFinder(args, args[0])
+
+    totalWC = 0
+    totalLC = 0
+
+    for file in args[1:]:
+        print(f"\nFicheiro: {file}\n")
+        wc, lc = matchFinder(file, args, args[0])
+        totalWC += wc
+        totalLC += lc
+    
 
     if any("-c" in opt for opt in opts):
-        print(f"Total de ocorrências: {wc}")
+        print(f"Total de ocorrências: {totalWC}")
 
     if any("-l" in opt for opt in opts):
-        print(f"Total de linhas: {lc}")
+        print(f"Total de linhas: {totalLC}")
 
     
 
 
-def matchFinder(args, word):
+def matchFinder(file, args, word):
 
     wc = 0
     lc = 0
+    
+    # se -p omitido ou 0
+    with open(file, "r") as f:
 
-    for file in args[1:]:
-        # se -p omitido ou 0
-        with open(file, "r") as f:
+        lines = f.readlines()
 
-            lines = f.readlines()
+        for lineIndex in range(len(lines)):
+            splitLine = lines[lineIndex].strip("\n").split()
+            if word in splitLine: #para que não apareçam palavras pegadas a outras
+                lc += 1
+                wc += splitLine.count(word)
 
-            for lineIndex in range(len(lines)):
-                splitLine = lines[lineIndex].strip("\n").split()
-                if word in splitLine: #para que não apareçam palavras pegadas a outras
-                    lc += 1
-                    wc += splitLine.count(word)
-                    print(lineIndex+1, lines[lineIndex].replace(word, f"{CRED}{word}{CEND}"))
+                
+
+                #print(lineIndex+1, lines[lineIndex].replace(" " + word + " ", f"{CRED} {word} {CEND}"))
+                
+                print(lineIndex+1, lines[lineIndex].replace(word, f"{CRED}{word}{CEND}")) #TODO: Fix wrong matches being highlighted
     
     return wc, lc
 
