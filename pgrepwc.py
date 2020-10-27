eb855f656ebf91abff2c6444f2cd3f14d648e681
Ha-sake <57680069+Ha-sake@users.noreply.github.com>
2020-10-27 20:13:26 +0000

    Attempt at load-balancing

--- pgrepwc.py
+++ pgrepwc.py
@@ -1,10 +1,11 @@
 import sys, getopt, os
+from math import ceil
 
 
-# Cor
-CRED = '\033[91m'
-CEND = '\033[0m'
-os.system('color')
+# # Cor
+# CRED = '\033[91m'
+# CEND = '\033[0m'
+# os.system('color')
 
 
 def main(argv):
@@ -24,9 +25,26 @@ def main(argv):
     totalWC = 0
     totalLC = 0
 
-    for file in args[1:]:
-        print(f"\nFicheiro: {file}\n")
-        wc, lc = matchFinder(file, args, args[0])
+    for opt in opts:
+        if opt[0] == "-p":
+            numberOfProcesses = opt[1]
+
+    allFiles = args[1:]
+
+
+    numberOfFilesPerProcess = ceil(len(allFiles)/numberOfProcesses)
+
+    while len(allFiles)>0:
+
+        filesToHandle = []
+
+        for i in range(numberOfFilesPerProcess):
+            
+            if i > len(allFiles)-1:
+                filesToHandle.append(allFiles.pop(i))
+        
+
+        wc, lc = matchFinder(filesToHandle, args, args[0])
         totalWC += wc
         totalLC += lc
     
@@ -37,31 +55,34 @@ def main(argv):
     if any("-l" in opt for opt in opts):
         print(f"Total de linhas: {totalLC}")
 
-    
 
 
-def matchFinder(file, args, word):
+
+def matchFinder(files, args, word):
 
     wc = 0
     lc = 0
     
-    # se -p omitido ou 0
-    with open(file, "r") as f:
+    for file in files:
+        # se -p omitido ou 0
+        with open(file, "r") as f:
 
-        lines = f.readlines()
+            print(f"\nFicheiro: {file}\n")
 
-        for lineIndex in range(len(lines)):
-            splitLine = lines[lineIndex].strip("\n").split()
-            if word in splitLine: #para que não apareçam palavras pegadas a outras
-                lc += 1
-                wc += splitLine.count(word)
+            lines = f.readlines()
 
-                
+            for lineIndex in range(len(lines)):
+                splitLine = lines[lineIndex].strip("\n").split()
+                if word in splitLine: #para que não apareçam palavras pegadas a outras
+                    lc += 1
+                    wc += splitLine.count(word)
 
-                #print(lineIndex+1, lines[lineIndex].replace(" " + word + " ", f"{CRED} {word} {CEND}"))
-                
-                print(lineIndex+1, lines[lineIndex].replace(word, f"{CRED}{word}{CEND}")) #TODO: Fix wrong matches being highlighted
-    
+                    
+                    print(lineIndex+1,lines[lineIndex])
+                    #print(lineIndex+1, lines[lineIndex].replace(" " + word + " ", f"{CRED} {word} {CEND}"))
+                    
+                    #print(lineIndex+1, lines[lineIndex].replace(word, f"{CRED}{word}{CEND}")) #TODO: Fix wrong matches being highlighted
+
     return wc, lc
 
                     
